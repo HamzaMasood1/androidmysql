e3282f52fc226b4d73c51f8fe0f1d1dfebe3b137
HamzaMasood1 <hamzamasood183@gmail.com>
2020-02-07 06:38:50 +0000

    first commit

---
+++ app/src/main/java/com/example/myapplication/URLS.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+public class URLS {
+    private static final String URL_ROOT = "http://192.168.0.248/test_android/api.php?action=";
+    public static final String URL_REGISTER = URL_ROOT + "signup";
+    public static final String URL_LOGIN= URL_ROOT + "login";
+}
